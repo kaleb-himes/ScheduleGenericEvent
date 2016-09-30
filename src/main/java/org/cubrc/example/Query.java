ec5b9174cebd2e2c8b939e9d53aae4382d0f98ee
robertweber95 <robertweber95@gmail.com>
2016-09-30 17:06:56 -0400

    Added day value to insert query

--- src/main/java/org/cubrc/example/Query.java
+++ src/main/java/org/cubrc/example/Query.java
@@ -9,8 +9,17 @@ import jdk.nashorn.internal.parser.JSONParser;
  */
 public class Query {
 
-    public void insertEvent(String event){
-        // TODO: Implement INSERT Query
+    public void insertEvent(Event event){
+        RestClient client = new RestClient();
+        client.setServerToQuery();
+        String query = "PREFIX ero: <http://www.ontologylibrary.mil/CommonCore/Upper/ExtendedRelationOntology#>" +
+                "PREFIX to: <http://www.ontologylibrary.mil/CommonCore/Mid/TimeOntology#>" +
+                "PREFIX agent: <http://www.ontologylibrary.mil/CommonCore/Mid/AgentOntology#>" +
+                "PREFIX info: <http://www.ontologylibrary.mil/CommonCore/Mid/InformationEntityOntology#>" +
+                "INSERT DATA { " +
+                "agent:Event ero:bearer_of to:day . " +
+                "to:day info:has_text_value " + event.getDay() + " ; } " ;
+        String response = client.post("", query);
     }
     public String selectEvent(){
         // TODO: Implement SELECT Query
